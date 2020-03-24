25cf3e7efa11f38ac7d8e6a8cbbc1df5bedfe9cf
athompson-tamu <athompson-tamu@github.com>
2020-03-24 01:44:02 -0500

    started form factor lookup table

--- laue.py
+++ laue.py
@@ -8,7 +8,7 @@ alpha = 1/137  # fine structure constant
 
 
 class LaueCrystal:
-    def __init__(self, thickness=5e10, proton_number=32, planar_dist=2.83,
+    def __init__(self, thickness=5e10, proton_number=6, planar_dist=2.83,
                  reciprocal_lattice=6.2, bragg_angle=0.16, lattice=(2,2,0)):
         self.r = planar_dist  # keV^-1
         self.qt = reciprocal_lattice  # keV
@@ -33,16 +33,12 @@ class LaueCrystal:
                     return 0
         else:
             return 0
-    
-    def get_ff_params(self):
-        # TODO: use self.z to do a lookup table
-        return (16, 6.37, 3.7, 3.68, 2.85, 0.2516, 11.45, 54.76)
 
     def q(self, t, e):
         return 4*pi*e*sin(2*t)
     
     def gamam_ff(self, q):
-        parameters=self.get_ff_params()
+        parameters = get_ff_params(self.z)
         a1 = parameters[0]
         a2 = parameters[1]
         a3 = parameters[2]
@@ -51,7 +47,7 @@ class LaueCrystal:
         b2 = parameters[5]
         b3 = parameters[6]
         b4 = parameters[7]
-        c = self.z - a1 - a2 - a3 - a4
+        c = parameters[8]
         return a1*exp(-b1*(q/(4*pi)**2)) + a2*exp(-b2*(q/(4*pi)**2)) \
                 + a3*exp(-b3*(q/(4*pi)**2)) + a4*exp(-b4*(q/(4*pi)**2)) + c
 
@@ -93,3 +89,18 @@ class LaueCrystal:
         D1 = self.eta(0, e) - self.u(t, e, ma) + self.dphi_gamma(t, e, ma) - 2*self.dphi_a(t, e, ma)
         D2 = self.eta(0, e) + self.u(t, e, ma) + self.dphi_gamma(t, e, ma) - 2*self.dphi_a(t, e, ma)
         return np.power(absolute(A * ((1 + B) * (C1 / D1) + (1 - B) * (C2 / D2))), 2)
+
+
+
+def get_ff_params(z):
+    if (z not in (1, 32, 6, 55)):
+        print("Element not found. You must enter it in by hand.")
+        return 1
+    if z == 1:
+        return (0.489918,20.6593,0.262003,7.74039,0.196767,49.5519,0.049879,2.20159,0.001305)
+    if z == 32:
+        return (16.0816,2.8509,6.3747,0.2516,3.7068,11.4468,3.683,54.7625,2.1313)
+    if z == 6:
+        return (2.31,20.8439,1.02,10.2075,1.5886,0.5687,0.865,51.6512,0.2156)
+    if z == 55:
+        return (20.3892,3.569,19.1062,0.3107,10.662,24.3879,1.4953,213.904,3.3352)
