fixed units to correct bloch resonance positions in mass space

test.py
@@ -10,7 +10,7 @@ energy = 20
 coupling = 1e-3
 
 
-ma_array = np.linspace(0.0, 0.05, 5000)
+ma_array = np.linspace(0.025, 0.05, 5000)
 theta_array = np.linspace(0.000001, pi/2, 5000)
 prim_array = np.ones(ma_array.shape[0]) * primakoff(coupling, 6)
 p_ma_array = np.zeros(ma_array.shape[0])